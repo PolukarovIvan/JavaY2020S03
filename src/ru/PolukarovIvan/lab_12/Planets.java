a95f2eef747a6e7fb2290577ca2f22ea6bf3143a
Ivan <you@example.com>
2020-11-03 17:25:00 +0300

    fix 12 lab

--- src/ru/PolukarovIvan/lab_12/Planets.java
+++ src/ru/PolukarovIvan/lab_12/Planets.java
@@ -1,19 +1,24 @@
 package ru.PolukarovIvan.lab_12;
 
 public enum Planets {
-    Mercury(2440 * (Math.pow(10, 3)), 3.33 * (Math.pow(10,23))),
-    Venus(6050 * (Math.pow(10, 3)), 4.88 * (Math.pow(10,24))),
-    Earth(6370 * (Math.pow(10, 3)), 5.976 * (Math.pow(10,24))),
-    Mars(3390 * (Math.pow(10, 3)), 6.43 * (Math.pow(10,23))),
-    Jupiter(71490 * (Math.pow(10, 3)), 1.9 * (Math.pow(10,27))),
-    Saturn(60270 * (Math.pow(10, 3)), 5.69 * (Math.pow(10,26))),
-    Uranus(25560 * (Math.pow(10, 3)), 8.69 * (Math.pow(10,25))),
-    Neptune(24760 * (Math.pow(10, 3)), 1.04 * (Math.pow(10,26)));
-    public double radius; //м
-    public double mass; //кг
+
+    Mercury(2440 * 1e3, 3.33 * 1e23),
+    Venus(6050 * 1e3, 4.88 * 1e24),
+    Earth(6370 * 1e3, 5.976 * 1e24),
+    Mars(3390 * 1e3, 6.43 * 1e23),
+    Jupiter(71490 * 1e3, 1.9 * 1e27),
+    Saturn(60270 * 1e3, 5.69 * 1e26),
+    Uranus(25560 * 1e3, 8.69 * 1e27),
+    Neptune(24760 * 1e3, 1.04 * 1e26);
+
+    public double radius;
+    public double mass;
 
     Planets(double radius, double mass) {
         this.radius = radius;
         this.mass = mass;
     }
+
+
+
 }
